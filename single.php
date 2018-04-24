2f4ece8e1847f28a30136b1bd479fa71fd16577b
vanevery <vanevery@mobil-eyes-us.localdomain>
2018-04-24 13:46:31 +0000

    Reverting

--- single.php
+++ single.php
@@ -1,13 +1,8 @@
 <?php
 /**
- * The template for displaying all pages
+ * The template for displaying all single posts
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
  * @package MEU
  */
@@ -15,26 +10,117 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
 
-		<?php
+<meta charset="UTF-8">
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	
+	
+
+</head>
+
+<body>
+
+<?php wp_head(); ?>
+<div class="wrap">
+
+<div class="videocontainer">
+	
+<div class="fb-video">
+<?php the_title( '<h2>', '</h2>' ); ?>
+
+<?php
 		while ( have_posts() ) :
 			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			the_content ('<p>','</p>');
 
 		endwhile; // End of the loop.
 		?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</div>
 
-<?php
-get_sidebar();
-get_footer();
+			<div class="tab">
+      							<input id="tab-one" type="checkbox" name="tabs">
+     							<label class="accordion-label" for="tab-one">
+     								<p>About this campaign</p>
+     								<i></i>
+     							</label>
+     							<div class="tab-content">
+									<p>
+						<?php
+						$categories = get_the_category();
+						echo category_description( $categories[0] ); ?> 
+												</p>
+									
+									</div>
+    </div>
+	</div>
+
+<!--
+<div class="context">
+
+<div id="childbox">
+<div id="buttonpanel">
+<button class="btn active" id="helptranslate" data-target="translation">Translate</button>
+  <button class="btn" id="givecontext" data-target="givingcontext">Give context</button>
+ </div>
+
+<div class="box" id="translation">
+<textarea name="text" class="translationbox"></textarea>
+<div class="submit">
+	<button  class="btn" type="submit" value="Submit">Submit</button>
+		</div>
+</div>
+
+<div class="box" id="givingcontext">
+<textarea name="text" class="translationbox"></textarea>
+<div class="submit">
+	<button  class="btn" type="submit" value="Submit">Submit</button>
+		</div>
+</div>
+
+</div>
+					
+	
+	<div id="primary">
+		<div id="content">
+
+	<div class="newcontentbuttons">
+		<div id="togglecontent">
+<button class="btn active" id="seetranslation" data-target="addedtranslation">Translation</button>
+  <button class="btn" id="seecontext" data-target="addedcontext">Context</button>
+		
+ </div>
+		<div id="refresh">
+			<button class="btn" onClick="history.go(0)" VALUE="Refresh">
+				<i style="font-size:18px" class="fa">&#xf021;</i>
+			</button>
+			</div>
+
+			</div>
+		<div class="clearfix">
+			</div>
+		<div class="addedcontent" id="addedtranslation">
+			Translation Should Go Here
+			</div>
+			
+			<div class="addedcontent" id="addedcontext">
+			Context Should go here
+			</div>
+
+		</div>
+	</div>
+
+	</div>
+</div>
+-->
+
+
+
